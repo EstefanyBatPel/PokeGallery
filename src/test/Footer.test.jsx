4a5c524e0e58122d9d9c1eadd02531cc6cf30760
EstefanyBatPel <estefanybatpel@gmail.com>
2024-02-29 13:18:03 +0100

    prepare archives

---
+++ src/test/Footer.test.jsx
@@ -0,0 +1,15 @@
+// import {render, screen} from "@testing-library/react";
+// import Footer from "../components/Footer";
+// import { expect, test, describe, beforeEach } from 'vitest';
+
+// describe("Footer testing", ()=>{
+//     beforeEach(()=> {
+//         render(<Footer/>)
+//     })
+
+// test("Footer have an instagram logo", () => { 
+//     const logo = screen.getByAltText("instagram");
+//     expect(logo).toBeDefined();
+// })
+
+// })
